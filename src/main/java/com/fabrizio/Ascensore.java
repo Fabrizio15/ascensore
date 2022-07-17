4fa84a6e5f77a2d807c3f55b7b09fd8871b7cc8d
Fabrizio15 <VRIFRZ01M11C351P@studium.unict.it>
2022-07-17 14:01:20 +0200

    aggiunte classi essenziali

--- src/main/java/com/fabrizio/Ascensore.java
+++ src/main/java/com/fabrizio/Ascensore.java
@@ -1,5 +1,44 @@
 package com.fabrizio;
 
+import java.util.ArrayList;
+
 public class Ascensore {
-    
+   
+    private static Ascensore ascensore = null;
+    private int maxWeight = 0;
+    private int currentWeight = 0;
+    private int currentFloor = 0;
+    //private ArrayList<Person> persons = new ArrayList<>()...
+
+    public int getCurrentWeight() {
+        return currentWeight;
+    }
+
+    public void setCurrentWeight(int currentWeight) {
+        this.currentWeight = currentWeight;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void setCurrentFloor(int currentFloor) {
+        this.currentFloor = currentFloor;
+    }
+
+    public int getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    //metodo getInstance del dp Singleton per Ascensore
+    public static Ascensore getAscensore() {
+        if(ascensore == null){
+            ascensore = new Ascensore();
+        }
+        return ascensore;
+    }
 }
