5c64228d273c3b3753716a1cfe81a28473a41054
Fabrizio15 <VRIFRZ01M11C351P@studium.unict.it>
2022-07-17 19:59:18 +0200

    bug fix

--- src/main/java/com/fabrizio/Progetto.java
+++ src/main/java/com/fabrizio/Progetto.java
@@ -3,10 +3,15 @@ package com.fabrizio;
 /**
  * Hello world!
  */
-public class App 
+public class Progetto   
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Persona p = new Persona();
+        p.setName("buongiorno").setWeight(100);
+
+
     }
+    
 }
